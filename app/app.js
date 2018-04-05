841ff73b4c7d547408c98ff1fc8c6c065c641f09
Tobias <Tobias.Rhrer@htl.rennweg.at>
2018-04-05 14:51:36 +0200

    Initial Commit

---
+++ app/app.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// Einziges Modul dieser App und seine Abhängigkeiten
+var app = angular.module("todoliste", [ "ngResource", "ngMessages", "ngLocale", "ngSanitize",
+    "ngAnimate", "ngMaterial", "ui.router" ]);
+
+
+// Einstellungen für Debugging
+app.config(function($logProvider, $compileProvider, $mdAriaProvider, $qProvider) {
+    $logProvider.debugEnabled(true);
+    $compileProvider.debugInfoEnabled(true);
+    $mdAriaProvider.disableWarnings();
+    $qProvider.errorOnUnhandledRejections(false);
+});
+
+
+// Datepicker auf AngularJS-Gebietsschema einstellen
+app.config(function($localeProvider, $mdDateLocaleProvider) {
+    var locale = $localeProvider.$get();
+
+    moment.locale(locale.id);
+
+    $mdDateLocaleProvider.months = moment.months();
+    $mdDateLocaleProvider.shortMonths = moment.monthsShort();
+    $mdDateLocaleProvider.days = moment.weekdays();
+    $mdDateLocaleProvider.shortDays = moment.weekdaysShort();
+    $mdDateLocaleProvider.firstDayOfWeek = locale.DATETIME_FORMATS.FIRSTDAYOFWEEK;
+
+    $mdDateLocaleProvider.parseDate = function(dateString) {
+        var m = moment(dateString, "L", locale.id);
+        return m.isValid() ? m.toDate() : new Date(NaN);
+    };
+
+    $mdDateLocaleProvider.formatDate = function(date) {
+        var m = moment(date);
+        return m.isValid() ? m.format("L") : "";
+    };
+
+    $mdDateLocaleProvider.monthHeaderFormatter = function(date) {
+        return `${moment.monthsShort()[date.getMonth()]}  ${date.getFullYear()}`;
+    };
+
+    $mdDateLocaleProvider.weekNumberFormatter = function(weekNumber) {
+        return `Woche ${weekNumber}`;
+    };
+
+    $mdDateLocaleProvider.msgCalendar = "Kalender";
+    $mdDateLocaleProvider.msgOpenCalendar = "Kalender öffnen";
+});
