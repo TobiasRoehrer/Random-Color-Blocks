2015a2542cb942f75183f9bed3af0ec2be7d649d
Tobias <Tobias.Rhrer@htl.rennweg.at>
2018-04-07 09:13:08 +0200

    minor bugfix in color.js

--- app/models/color.js
+++ app/models/color.js
@@ -2,15 +2,9 @@ app.factory("color", function () {
     function color(red,green,blue) {
         let me = this;
 
-        this.red = Math.floor(Math.random()*256);
-        this.green = Math.floor(Math.random()*256);
-        this.blue = Math.floor(Math.random()*256);
-
-        //The following block is not necessary and also faulty.
-        /*
-        this.red = (red < 256 && red >= 0) ? red : (Math.floor(Math.random()*256));
-        this.green = (green < 256 && green >= 0) ? green : (Math.floor(Math.random()*256));
-        this.blue = (blue < 256 && blue >= 0) ? blue : (Math.floor(Math.random()*256));
+        this.red = (red && red < 256 && red >= 0) ? red : (Math.floor(Math.random()*256));
+        this.green = (green && green < 256 && green >= 0) ? green : (Math.floor(Math.random()*256));
+        this.blue = (blue && blue < 256 && blue >= 0) ? blue : (Math.floor(Math.random()*256));
 
         Object.defineProperty(this, red, {writeable: false});
         Object.defineProperty(this, green, {writeable: false});
@@ -19,7 +13,6 @@ app.factory("color", function () {
         this.variant = function({red = me.red, green = me.green, blue = me.blue}){
             return new color(red,green,blue);
         }
-        */
 
         this.string = function(){
             let redhex = ("0" + me.red.toString(16)).substr(-2).toUpperCase();
